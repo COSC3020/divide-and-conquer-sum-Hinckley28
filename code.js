efe71a81fc38d4ba76462076bb31e9734e88d637
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-02-21 15:53:08 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
-function divideAndConquerSum(a) {
-    return a;
-}
+function helper(arr) {
+    if (arr.length <= 3) {
+      return arr.reduce((sum, num) => sum + num, 0); 
+    }
+
+    let n = Math.floor(arr.length / 3);
+    let leftSum = helper(arr.slice(0, n));
+    let midSum = helper(arr.slice(n, 2 * n));
+    let rightSum = helper(arr.slice(2 * n));
+    return leftSum + midSum + rightSum;
+  }
+
+function divideAndConquerSum(arr) {
+  
+    return helper(arr);
+  }
